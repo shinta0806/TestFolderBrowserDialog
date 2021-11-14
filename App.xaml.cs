3754085b8ec34c8f2034155e246d5fc7f67b4175
shinta0806 <shinta.0806@gmail.com>
2021-11-14 12:39:57 +0900

    プロジェクト ファイルを追加します。

---
+++ App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Livet;
+
+using System;
+using System.Windows;
+
+namespace TestFolderBrowserDialog
+{
+	public partial class App : Application
+	{
+		private void Application_Startup(object sender, StartupEventArgs e)
+		{
+			DispatcherHelper.UIDispatcher = Dispatcher;
+			AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+		}
+
+		// Application level error handling
+		private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+		{
+			String message = "Something errors were occurred.";
+			if (e.ExceptionObject is Exception excep)
+			{
+				message += "\n" + excep.Message;
+			}
+			MessageBox.Show(message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+			Environment.Exit(1);
+		}
+	}
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Livet;
+using Livet.Messaging.IO;
+
+using System.Windows;
+
+namespace TestFolderBrowserDialog.ViewModels
+{
+	public class MainWindowViewModel : ViewModel
+	{
+		public void AddFolderSelected(FolderSelectionMessage folderSelectionMessage)
+		{
+			MessageBox.Show(folderSelectionMessage.Response[0]);
+		}
+
+		// Some useful code snippets for ViewModel are defined as l*(llcom, llcomn, lvcomm, lsprop, etc...).
+		public void Initialize()
+		{
+		}
+	}
+}
