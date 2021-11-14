57eab3e7bbf4ab1f97fb5d181c70396e64ac6ebc
shinta0806 <shinta.0806@gmail.com>
2021-11-14 12:47:02 +0900

    .NET 5

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -9,7 +9,14 @@ namespace TestFolderBrowserDialog.ViewModels
 	{
 		public void AddFolderSelected(FolderSelectionMessage folderSelectionMessage)
 		{
-			MessageBox.Show(folderSelectionMessage.Response[0]);
+			if (folderSelectionMessage.Response == null)
+			{
+				MessageBox.Show("キャンセル");
+			}
+			else
+			{
+				MessageBox.Show(folderSelectionMessage.Response[0]);
+			}
 		}
 
 		// Some useful code snippets for ViewModel are defined as l*(llcom, llcomn, lvcomm, lsprop, etc...).
